76e760fb474bbb91cc0aa63a86e6b54d4dc9a9ed
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2023-12-03 17:32:37 -0600

    stpx module specific var uses implemented start
    conditional int purposes to construct of stpx module's future usage with SolaceXN ai interlink communication via bluetooth or other

--- PySqTpp_SarInterface.py
+++ PySqTpp_SarInterface.py
@@ -1,7 +1,7 @@
 # Code File: StaqTapp-1.02 [PySqTpp_SarInterface.py] StaqTapp S.A.R. main interface class
 
 
-# Staqtapp 1.02.385
+# Staqtapp 1.02.387
 
 # email: 5deg.blk.blt.cecil(@)gmail
 # github: https://github.com/lastforkbender/staqtapp
--- stpx.py
+++ stpx.py
@@ -1,7 +1,7 @@
 # Code File: StaqTapp-1.02 [stpx.py] StaqTapp gzip methods & module calls
 
 
-# Staqtapp 1.02.385
+# Staqtapp 1.02.387
 
 # email: 5deg.blk.blt.cecil(@)gmail
 # github: https://github.com/lastforkbender/staqtapp
@@ -65,91 +65,176 @@ def x_getpath(pth: str) -> list:
         print("staqtapp stpx error: ", e)
 #______________________________________________________________________________________
 
-def x_makesource(isStatic, isMakeFolder, isEraseSource):
+def x_tweedvar(isGreater: bool, isSar: bool, varName: str):
+    # returns a palindrome set number from a number(s), @varName's data
+    # if @isGreater=True then arranges the number(s) greatest inward to outer
+    # this method will always return a twin 3-digit number regardless of original;
+    # no from zero escaping a [ONE] placehold or a "man-mon"-AC nonexist trust;
+    # no zero pairing is set most inward or most outward of branching [ONE]
+    # if any number(s) are of a decimal notation, returns None or None in a list;
+    # of @varName is sar variable name then returns that name dsg as a sweed
+    # does not return the sar variable's number data reconstructed if any digits
+    # calls pilot function stpp.loadvar_str() without the needed parameters
+    # of a folder & file --> .tqpt source path @x_setpath() as already set
+    nSt = set('-0123456789')
+    stLst = []
+    dtLst = []
+    lStCnt = 0
+    rStCnt = 0
+    cnt = 0
+    if isSar == False:
+        pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
+        rslt = stpp.loadvar_str(varName, pth[0], pth[1])
+        if isinstance(rslt, list) == True:
+            
+        else:
+            if set(rslt).issubset(nSt) == True:
+                for x in rslt:
+                    if x != '-':
+                        # first of this menorah type counting, how many +1 or -1 loops?
+                        # ...and cache the retains of pairing slots encountered to digits
+                        # if more placeholds determined, not a simple fade count then
+                        # each multiple look(loop) ahead or behind is of < > equal outs
+                        # menorah always cHalf rotational +/-loop to a whool loop [ONE]
+                        # of counting multi-loop any digit, not single loop aka 'nero-zero'
+                        # by which thru fear of nonexist(nero-zero) has caused too many
+                        # lies, thefts, murders, wars, ongoing destroy of earth [manmon]
+                        if int(x) == int(prvX)-1: lStCnt+=1
+                        elif int(x) == int(prvX)+1: lStCnt-=1
+                        elif int(x)-1 == int(prvX): rStCnt+=1
+                        elif int(x)+1 == int(prvX): rStCnt-=1
+                        if lStCnt == rStCnt:
+                            lStCnt = 1
+                            rStCnt = 1
+                        if x == prvX:
+                            stLst.append(x)
+                        else:
+                            dtLst.append(x)
+                        prvX = x
+                        cnt+=1
+                # qx = central/rotational-apparent position alpha observed
+                qx = None
+                # Chai'ohm: one placehold from One, inescapable loop exist via
+                # resistance as utterly already all now established from All(El),
+                # or as time(new) as resistance you cannot change to finite loop
+                # as and/or a dead stop nothing/nero-zero of time(future=past)
+                # a absolute impossible conclusion of any quantum reality no
+                # matter the education level or any amount of manmon trusted
+                # .......or from destructive ignorant pride aka time = money
+                if lStCnt == rStCnt: qx = 1
+                elif cnt == 1: qx = 1
+                else:
+                    cLen = None
+                    dtLen = len(dtLst)
+                    stLen = len(stLst)
+                    dtGt = False
+                    if dtLen > stLen:
+                        dtGt = True
+                        cLen = dtLen
+                    else: cLen = stLen
+                    for y in range(cLen):
+                        # determine which numbers(loops) are of a greater farthest or least
+                        # farthest from the least or greater number pairing already observed;
+                        # this single pairing is always chai'ohm to a previous pair expanding
+                        # inescapable to one placeholding resolve, @cnt used as a delimiter
+                        # of any LtoR or RtoL nested accumlate further compressions, this
+                        # may be complicated to understand because of the teachings non-
+                        # exist actually exist, which was the next purposed lie in Eden after
+                        # the lie you are smarter.... to cause fear of nonexist; fear of non-
+                        # exist is also the most dangerous concept a AI can take on
+            else:
+                # @varName's data, invalid chars for a tweedvar() palindrome number return
+                return None
+    else:
+        # check if a proper staqtapp sar variable naming, of a unique ten digit dsg
+    
+#______________________________________________________________________________________
+
+def x_makesource(isStatic: bool, isMakeFolder: bool, isEraseSource: bool):
     # calls pilot function stpp.makesource() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.makesource(isStatic, isMakeFolder, isEraseSource, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_addvar(varName, varData):
+def x_addvar(varName: str, varData: str):
     # calls pilot function stpp.addvar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.addvar(varName, varData, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_renamevar(varName, newVarName):
+def x_renamevar(varName: str, newVarName: str):
     # calls pilot function stpp.renamevar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.renamevar(varName, newVarName, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_loadvar_str(varName):
+def x_loadvar_str(varName: str):
     # calls pilot function stpp.loadvar_str() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     return stpp.loadvar_str(varName, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_loadvar_deque(isNumbers, varName):
+def x_loadvar_deque(isNumbers: bool, varName: str):
     # calls pilot function stpp.loadvar_deque() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     return stpp.loadvar_deque(isNumbers, varName, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_loadsar_remap(isPrmExc, catPin):
+def x_loadsar_remap(isPrmExc: bool, catPin: str):
     # calls pilot function stpp.loadsar_remap() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.loadsar_remap(isPrmExc, catPin, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_changevar(varName, newVarData):
+def x_changevar(varName: str, newVarData: str):
     # calls pilot function stpp.changevar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.changevar(varName, newVarData, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_findvar(allSources, varName):
+def x_findvar(allSources: bool, varName: str):
     # calls pilot function stpp.findvar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     return stpp.findvar(allSources, varName, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_addsar(catPin, varData):
+def x_addsar(catPin: str, varData: str):
     # calls pilot function stpp.addsar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.addsar(catPin, varData, pth[0], pth[1])
 #______________________________________________________________________________________
 
-def x_lockvar(varName, fncName):
+def x_lockvar(varName: str, fncName: str):
     # calls utility function stpp.lockvar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     stpp.lockvar(varName, fncName, pth[0] + '/' + pth[1] + '.tqpt')
 #______________________________________________________________________________________
 
-def x_keyvar(isLogF, varName, fncName):
+def x_keyvar(isLogF: bool, varName: str, fncName: str):
     # calls utility function stpp.keyvar() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     return stpp.keyvar(isLogF, varName, fncName, pth[0] + '/' + pth[1] + '.tqpt')
 #______________________________________________________________________________________
 
-def x_searchkeys(varName):
+def x_searchkeys(varName: str):
     # calls utility function stpp.searchkeys() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
     return stpp.searchkeys(varName, pth[0] + '/' + pth[1] + '.tqpt')
 #______________________________________________________________________________________
 
-def x_lockvar_edit(varName, fncName):
+def x_lockvar_edit(varName: str, fncName: str):
     # calls utility function stpp.lockvar_edit() without needed parameters
     # of a folder & file --> .tqpt source path @x_setpath() as already set
     pth = x_getpath(os.path.dirname(os.path.abspath(__file__)) + '/stpx/x_stpx.gz')
