44e1329d84b3269bc321ffcff9adbacb49cb5ee5
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-01-01 15:34:35 -0600

    Create PySqTpp_Koch_Codex_Tumbler.py
    Add of the Staqtapp-Koch version module codex tumbler

---
+++ PySqTpp_Koch_Codex_Tumbler.py
@@ -0,0 +1,104 @@
+# __________________________________________________________________________________
+
+# Staqtapp-Koch: Hybrid Env-Vars Lib (https://github.com/lastforkbender/staqtapp)
+
+# Version: 2.01.024
+# __________________________________________________________________________________
+
+
+# ●■ Staqtapp-Koch Module Name: PySqTpp_Koch_Codex_Tumbler.py
+
+
+# ● ■  Description of this module's purpose:
+    
+#       This module a list str oriented palindrome probable access returns.
+#       Can handle deep obfuscation probables quickly for outside resolves.
+#       Also can be easily integrated of qubit like behaviors with pairing
+#       tuples; and if probables inserted to a proper uneven split regions.
+#       (These encodings are considered of security value & non-commented)
+
+
+# ● ■  Staqtapp-Koch Env-Vars Library Overview:
+
+#       The scope of this env-vars system is routine focused upon env-var
+#       security via obfuscated addresses inter-connected to env-vars own
+#       obfuscation of it's data, using tor like circumstances, procedural
+#       generation of env-var keys/spacing and one-way shared descriptors.
+
+#       Having limited made keys outside this env-var system for access of
+#       advanced security issue... if chosen of a needed env-vars solution.
+#       Of then a email response will be given for a phone number to call,
+#       involving a signature and purchase after agreed terms are met, then
+#       custom built Staqtapp-Koch full package library for your key sent.
+#       Pricing terms for single package is $318 U.S. dollars, however ---
+#       is variable to the conditions of use.(This public version does not
+#       include those strong fractal-palindrome based security modules.)
+
+    
+# Contact: rcttcr5@gmail.com
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+
+# Imported core python module(s) for this module's objectives.
+from random import choice
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+def slots_func(n):
+
+    if n % 2 == 0:
+        return choice(palindrome_set)
+    else:
+        return None
+# __________________________________________________________________________________
+
+def is_palindrome(word):
+
+    return word == word[::-1]
+# __________________________________________________________________________________
+
+class BaseClass:
+
+    def __init__(self, branches):
+        self.branches = branches
+        self.branch_values = {i: slots_func(i) for i in range(branches)}
+
+    def get_branch_values(self):
+        return self.branch_values
+
+    def set_branch_values(self, value):
+        self.branch_values = value
+
+    def update_branch_values(self, index, value):
+        self.branch_values[index] = value
+
+    def reset_branch_values(self):
+        self.branch_values = {i: slots_func(i) for i in range(self.branches)}
+# __________________________________________________________________________________
+
+class NestedClass(BaseClass):
+    def __init__(self, branches, values):
+        super().__init__(branches)
+        self.values = values
+
+    def update_values(self, value):
+        self.values = value
+# __________________________________________________________________________________
+
+class BranchedNestedClass(NestedClass):
+
+    def __init__(self, branches, values):
+        super().__init__(branches, values)
+        self.probabilities = {}
+        self.reset_probabilities()
+
+    def update_probabilities(self, value):
+
+        self.probabilities = value
+
+    def reset_probabilities(self):
+
+        self.probabilities = {i: slots_func(i) for i in range(self.branches)}
