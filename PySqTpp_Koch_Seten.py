0c8bf4c1c32d1efdcd2f257d66bb40c92ca4eafc
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-01-08 03:49:36 -0600

    main seten module added
    staqtapp-koch director module seten added

---
+++ PySqTpp_Koch_Seten.py
@@ -0,0 +1,89 @@
+# __________________________________________________________________________________
+
+# Staqtapp-Koch: Hybrid Env-Vars Lib (https://github.com/lastforkbender/staqtapp)
+
+# Version: 2.01.028
+# __________________________________________________________________________________
+
+
+# ●■ Staqtapp-Koch Module Name: PySqTpp_Koch_Seten.py
+
+
+# ● ■  Description of this module's purpose:
+    
+#       This module directs all Seten related modules, including types of
+#       key sets, total obfuscation+encryption layering of env-vars data.
+#       However is disconnected from what random obfuscation was applied;
+#       in that attempts of hijacking this module's direction of what ---
+#       obfuscation next or previous is extreme entanglement both ways.
+#       Also uses sar type variables and sar type pointers, whereof env-
+#       vars have no specific name, just a address of random numbers. Of
+#       this type addr pointing once compressed, not even the most power-
+#       ful quantum computers in the future can sort them correctly again
+#       unless both compression table constants known, no invert nesting.
+#       (See method extr_load_sar_remap() @ PySqTpp_Alpha.py of Staqtapp)
+
+
+# ● ■  Staqtapp-Koch Env-Vars Library Overview:
+
+#       The scope of this env-vars system is routine focused upon env-var
+#       security via obfuscated addresses inter-connected to env-vars own
+#       obfuscation of it's data, using tor like circumstances, procedural
+#       generation of env-var keys/spacing and one-way shared descriptors.
+
+#       Having limited made keys outside this env-var system for access of
+#       advanced security issue... if chosen of a needed env-vars solution.
+#       Of then a email response will be given for a phone number to call,
+#       involving a signature and purchase after agreed terms are met, then
+#       custom built Staqtapp-Koch full package library for your key sent.
+#       Pricing terms for single package is $318 U.S. dollars, however ---
+#       is variable to the conditions of use.(This public version does not
+#       include those strong fractal-palindrome based security modules.)
+
+    
+# Contact: rcttcr5@gmail.com
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+
+# Imported core python module(s) for this module's objectives.
+import threading
+
+# Imported PySqTpp_Koch module(s) for this module's objectives.
+import SqTpp_Koch_MasterKey as stkm
+
+
+class Seten(type):
+    
+    def __new__(self,_,__,___):
+        ___['_lock'] = threading.Lock()
+        return super(Seten, self).__new__(self,_,__,___)
+
+    def __init__(self,____,_____):
+        self._____=_____
+        self.____=____
+# __________________________________________________________________________________
+        
+class Staq(object):
+
+    __metaclass__ = Seten
+    
+    def __init__(self):
+        Seten._master_key = self.kch_set_master_key()
+        
+    @classmethod
+    def kch_get_lock(Seten):
+        return Seten._lock
+        
+    @classmethod
+    def kch_set_master_key(self):
+        # get initial master key for load start of initial env-vars file
+        # apply, return in split reads to first random seten sub-module
+        # run (the first key must resolve a %prd limit of a next mk key)
+        return 'pass'
+   
+# __________________________________________________________________________________
+        
+        
