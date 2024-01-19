79fdcd8da3142489dbda0239aa88ad15f713cf23
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-01-18 21:00:12 -0600

    PySqTpp_Koch_ThorHandle.py added for seten addrs
    Module handles seten module addr shifting core resolves once mk active to first chosen seten module

---
+++ PySqTpp_Koch_ThorHandle.py
@@ -0,0 +1,100 @@
+# __________________________________________________________________________________
+
+# Staqtapp-Koch: Hybrid Env-Vars Lib (https://github.com/lastforkbender/staqtapp)
+
+# Version: 2.01.028
+# __________________________________________________________________________________
+
+
+# ●■ Staqtapp-Koch Module Name: PySqTpp_Koch_ThorHandle.py
+
+
+# ● ■  Description of this module's purpose:
+    
+#       This module handles seten module addr shifting, returns mostly a
+#       series of int elements -|+ in range 0-9; is predictable and very
+#       unpredictable at the same time. Length list return is 32 numbers.
+#       (These encodings are considered of security value & non-commented)
+
+
+# ● ■  Staqtapp-Koch Env-Vars Library Overview:
+
+#       The scope of this env-vars system is routine focused upon env-var
+#       security via obfuscated addresses inter-connected to env-vars own
+#       obfuscation of it's data, using tor like circumstances, procedural
+#       generation of env-var keys/spacing and one-way shared descriptors.
+
+#       Having limited made keys outside this env-var system for access of
+#       advanced security issue... if chosen of a needed env-vars solution.
+#       Of then a email response will be given for a phone number to call,
+#       involving a signature and purchase after agreed terms are met, then
+#       custom built Staqtapp-Koch full package library for your key sent.
+#       Pricing terms for single package is $318 U.S. dollars, however ---
+#       is variable to the conditions of use.(This public version does not
+#       include those strong fractal-palindrome based security modules.)
+
+    
+# Contact: rcttcr5@gmail.com
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+
+# Imported core python module(s) for this module's objectives.
+import random
+import time
+import math
+# __________________________________________________________________________________
+
+class SqtppKochThorAddrHandler:
+    
+    def __init__(self,_,__,___,____,________):
+        self.____=____
+        self._=_+random.randint(99, 9999)
+        self._____= []
+        self.______=self.__________(___)
+        self.__=__
+        self.________=________  
+# __________________________________________________________________________________
+
+    def __________(self,_):
+        _=_+random.randint(99, 99999)
+        for _ in range(self._+_):
+            __=math.floor(_+math.tan(_-1*(_+math.sin(_*self._+self.____)))*math.floor(math.tan(_+1)))
+            if self.____________(__):
+                return _           
+# __________________________________________________________________________________
+
+    def ____________(self,_):
+        if _<2:
+            return False
+        for __ in range(2, int(_**0.5)+1):
+            if _%__==0:
+                return False
+        return True 
+# __________________________________________________________________________________
+
+    def _____________(self,_,__):
+        ___=__(_<<1)
+        ____=self.__________(math.ceil(_*2))
+        self._____.append(math.ceil(math.tan(_*(math.cos(___*_//2))-____)))   
+# __________________________________________________________________________________
+
+    def _______________(self,_):
+        __ = time.time()
+        if _ in self.__:
+            self.__[_]+=self.________+(__)-self.__[_][1]
+        else:
+            _____=self.__________(99999)
+            self.__[_]=[self.______+_,math.tan(math.tan(_+_))]
+        return int(self.__[_][0]*self.__[_][1])%_+_____    
+# __________________________________________________________________________________
+
+def sqtpp_koch_get_addr_shift_map(lvh, lve, lvk, lvf):
+    
+    lv = SqtppKochThorAddrHandler(lvh,{'T1':time.time(),'T2':time.time(),'T3':time.time()},lve,lvk,lvf)
+    for i in range(0,32):
+        lv._____________(lv._______________(1<<i),lambda x:x<<i)
+    return lv._____
+
