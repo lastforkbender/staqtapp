ec44754bb790f3df22b750eac93dccbd42a52e56
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-02-14 14:27:58 -0600

    Added PySqTpp_Koch_DCY decoy module
    Added PySqTpp_Koch_DCY decoy module

---
+++ PySqTpp_Koch_DCY.py
@@ -0,0 +1,64 @@
+# __________________________________________________________________________________
+
+# Staqtapp-Koch: Hybrid Env-Vars Lib (https://github.com/lastforkbender/staqtapp)
+
+# Version: 2.01.079
+# __________________________________________________________________________________
+
+
+# ●■ Staqtapp-Koch Module Name: PySqTpp_Koch_DCY.py
+
+
+# ● ■  Description of this module's purpose:
+    
+#       This module makes decoy/dummy vfs directories & files. This is the
+#       public version of this feature, contained in this module only. The
+#       more secure version of Staqtapp-Koch uses ml and urls for this. Is
+#       for protecting the .envfs file upfront; the code is commented here.
+#       However the more secure version of this library is not freeware.
+
+
+# ● ■  Staqtapp-Koch Env-Vars Library Overview:
+
+#       The current project status is critical to implementing security
+#       values of env-var use not seen before or done before. In focus
+#       of forthcoming advances, this env-var library is being built of
+#       a proposed system that forms a basis of complex abstractions to
+#       isolate those security values. Whereof any considerations into
+#       env-var security that would involve other discrete methods inact
+#       to placements of env-var use, those outer models of access or no.
+#       This library using fractal based rotational palindrome settings
+#       of env-var use and of extreme cut-off for modern ai comprehension
+#       if applied correctly as a modular sub-system approach.
+
+    
+# Contact: rcttcr5@gmail.com
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+
+# Imported core python module(s) for this module's objectives.
+import random
+
+
+class DCY:
+    
+    
+    def _mkdcy(self, isEnc: bool, isDF: bool, isDir: bool, isFl: bool, src: str):
+        # Either is a empty decoy directory, decoy directory and
+        # a empty file, or decoy directory & decoy file(@isDF)
+        # (@src str is used for the file's contents if as given)
+        # If isEnc then is real encryption applied, yet random.
+        # This ver. does not do any encryption of @src input; or
+        # use any type machine learning in that feature further.
+        if isDF:
+            pass
+        else:
+            if isD:
+                pass
+            else:
+                pass
+                
+        
