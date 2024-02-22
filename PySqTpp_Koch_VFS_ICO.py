eb708a80ffccb37e9f35fe729750e01fbb2f6771
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-02-22 16:00:25 -0600

    *Added PySqTpp_Koch_VFS_ICO.py
    *Added PySqTpp_Koch_VFS_ICO.py

---
+++ PySqTpp_Koch_VFS_ICO.py
@@ -0,0 +1,87 @@
+# __________________________________________________________________________________
+
+# Staqtapp-Koch: Hybrid Env-Vars Lib (https://github.com/lastforkbender/staqtapp)
+
+# Version: 2.01.079
+# __________________________________________________________________________________
+
+
+# ●■ Staqtapp-Koch Module Name: PySqTpp_Koch_VFS_ICO.py
+
+
+# ● ■  Description of this module's purpose:
+    
+#       This module is a remote protection for vfs module; and each seten
+#       module will not work without it present. Depending on the params
+#       and the zero-day start of this module, admin can track where this
+#       module began 'what are you doing!' and which master key was copy.
+
+
+# ● ■  Staqtapp-Koch Env-Vars Library Overview:
+
+#       The current project status is critical to implementing security
+#       values of env-var use not seen before or done before. In focus
+#       of forthcoming advances, this env-var library is being built of
+#       a proposed system that forms a basis of complex abstractions to
+#       isolate those security values. Whereof any considerations into
+#       env-var security that would involve other discrete methods inact
+#       to placements of env-var use, those outer models of access or no.
+#       This library using fractal based rotational palindrome settings
+#       of env-var use and of extreme cut-off for modern ai comprehension
+#       if applied correctly as a modular sub-system approach.
+
+    
+# Contact: rcttcr5@gmail.com
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+
+# Imported core python module(s) for this module's objectives.
+import threading
+import socket
+import types
+import sys
+
+class ICO_MNGR:
+    
+    def __init__(self):
+        self._______=[]
+        self._____=[]
+# __________________________________________________________________________________
+
+    def _____________(self,______):
+        ____=types.SimpleNamespace()
+        ____._________=______
+        ____.________={}
+        ____._______,_=socket.socketpair()
+        ____.thread=threading.Thread(target=self._______________,args=(____,),daemon=True)
+        ____.thread.start()
+        self._______.append(____)
+        self._____.append(____._______)
+        return ____
+# __________________________________________________________________________________
+
+    def _______________(self,_____):
+        sys.stderr=_____._________.__________('wb',buffering=0)
+        sys.stdout=_____._________.__________('wb', buffering=0)
+        sys.modules['__main__']=types.ModuleType('__main__')
+        sys.modules['__main__'].__dict__.update(_____.________)
+        sys.path.insert(0,'')
+        code = compile('def run():\n'+'\n'.join(_____.________.get('___lock_','vfs_ico.log')),'<string>','exec')
+        globals = {'run':run}
+        run()
+# __________________________________________________________________________________
+
+    def ____________(self,___,_____,_______):
+        ___.________[_____]=_______
+# __________________________________________________________________________________
+
+def _vfs_hlt(____,______):
+    print(f'what are you doing! {____}: {______}')
+    __________=ICO_MNGR()
+    ________=__________._____________('___')
+    __________.____________(________,None,print('_'))
+    print('*\n*\n*\n*\n*\n_')
+    
