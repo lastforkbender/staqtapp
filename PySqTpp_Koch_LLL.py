78d38e543c1a2adc9ebd94a1c3c08da606ca5260
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-02-23 23:24:05 -0600

    Added PySqTpp_Koch_LLL.py
    Added PySqTpp_Koch_LLL.py control module for ksms request or insertions

---
+++ PySqTpp_Koch_LLL.py
@@ -0,0 +1,152 @@
+# __________________________________________________________________________________
+
+# Staqtapp-Koch: Hybrid Env-Vars Lib (https://github.com/lastforkbender/staqtapp)
+
+# Version: 2.01.079
+# __________________________________________________________________________________
+
+
+# ●■ Staqtapp-Koch Module Name: PySqTpp_Koch_LLL.py
+
+
+# ● ■  Description of this module's purpose:
+    
+#       This module ...
+
+
+# ● ■  Staqtapp-Koch Env-Vars Library Overview:
+
+#       The current project status is critical to implementing security
+#       values of env-var use not seen before or done before. In focus
+#       of forthcoming advances, this env-var library is being built of
+#       a proposed system that forms a basis of complex abstractions to
+#       isolate those security values. Whereof any considerations into
+#       env-var security that would involve other discrete methods inact
+#       to placements of env-var use, those outer models of access or no.
+#       This library using fractal based rotational palindrome settings
+#       of env-var use and of extreme cut-off for modern ai comprehension
+#       if applied correctly as a modular sub-system approach.
+
+    
+# Contact: rcttcr5@gmail.com
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+# __________________________________________________________________________________
+
+
+# Imported core python module(s) for this module's objectives.
+import string as sg
+import random as rm
+
+# Imported PySqTpp_Koch module(s) for this module's objectives.
+from PySqTpp_Koch_ThorHandle import sqtpp_koch_get_addr_shift_map
+
+
+class LambdaLambdaLambda:
+    __slots__ = ('stn','txt','num','wrdlst','rsvlst')
+    
+    def __init__(self, stn, txt, num, wrdlst, rsvlst):
+        self.stn = {k:rm.randint(10000,99999) for k in rm.sample(sg.ascii_letters,20)}
+        self.txt = txt
+        self.num = num
+        self.wrdlst = wrdlst
+        self.rsvlst = rsvlst
+# __________________________________________________________________________________
+    
+    def lll_dirty_booger(self):
+        self.rsvlst = []
+        lmd = lambda:''.join(rm.choice(list(self.stn.keys())) for k in range(20))
+        for x in range(30): self.rsvlst.append(lmd())
+        return self.rsvlst
+# __________________________________________________________________________________
+        
+    def lll_nasty_booger(self):
+        wrd = self.wrdlst[rm.randint(0,len(self.wrdlst)-1)]
+        vrt = self.lll_vrt_stn_adr()
+        if wrd=='matssia' or wrd=='wyar':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:matssia-wyar:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='yatba' or wrd=='anlatdua':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:yatba-anlatdua:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='xem' or wrd=='jahua':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:xem-jahua:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='qege' or wrd=='dafm':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:qege-dafm:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='vruea' or wrd=='aqeb':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:vruea-aqeb:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='znof' or wrd=='beueh':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:znof-beueh:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='mosoy' or wrd=='phomtk':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:mosoy-phomtk:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        elif wrd=='yohb' or wrd=='ayac':
+            return f'{vrt[rm.randint(0,len(vrt)-1)]}:yohb-ayac:{sqtpp_koch_get_addr_shift_map(self.num[0],self.num[1],self.num[2],self.num[3])}'
+        
+    
+
+
+
+
+# __________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
