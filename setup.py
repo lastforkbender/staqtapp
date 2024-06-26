08f4d16cdd84f8fc115339bbb02b01df61d8aedf
lastforkbender <146335780+lastforkbender@users.noreply.github.com>
2024-06-26 12:19:43 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,76 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Staqtapp',
+    version = '1.2.222',
+    packages = ['Staqtapp'],
+    py_modules = ['staqtapp'],
+    author = 'Sir Robert Cecil',
+    author_email = 'rcttcr5@gmail.com',
+    description = 'Multi-integrated slots performance vfs env-var library system',
+    long_description = r"""
+     ____________________________________________________________________________________
+     
+        *See full list of import functions descriptions for Staqtapp 1.2 @ imports.txt
+        in this py package: Full descriptions & tips on certain embedded vfs responses.
+        
+    
+    
+    
+            Staqtapp 1.2 is an env-var library integrated for many options in env-var
+        use and research. All slots attrs programming throughout, featured importable
+        functions like stalkvar and joinvars redefine the power of using a vfs system
+        for env-var integrations. This library includes unique emphasis in allowing a
+        host variables assignments changes with stalkvar function, including searches
+        and direct/indirect changes of a stalked env-var or spawned var attached to a
+        stalked env-var. Opening up vast recursive options within the vfs file, that
+        can also be respawned itself. However is taken a step further... Staqtapp 1.2
+        has a custom tree dict type env-var, mv-tree, that has similiar responses as
+        a stalked env-var in spawning merged branching and merged key-pairing shifts.
+        
+     ____________________________________________________________________________________
+       ________________________________________________________________________________
+        ______________________________________________________________________________
+        
+            With Staqtapp 1.2, the option of qubit type simulation programming exist
+        by these importable functions: stalkvar, joinvars, changevar, addtree_stx,
+        addbranch_stx, getbranch_stx, revar and findvar_stx with property exchanges
+        of what env-var is multi-accessed or not via keyvar & lockvar functions. It
+        setups implementations of gate splicing across the env-var stacks in multiple
+        vfs files if chosen, where merge branch & shift features of a mv-tree usage.
+        These features added and tested for the Staqtapp-Koch 2nd version, which is a
+        multi-encrypt tor like vfs env-var lib of high level mobile security purposes.
+        Though a mv-tree env-var type for this Staqtapp version lacks some of those
+        requirements, is still the basic hard coded dict type re-structuring response
+        unchanged. The renamevar_stx with stalkvar functional intersections proves it.
+        
+     ____________________________________________________________________________________
+       ________________________________________________________________________________
+        ______________________________________________________________________________
+        
+            The lambdavar function is most recent added feature to Staqtapp 1.2 lib.
+        It works in a cojunction with addvar to recognize a lambda function is being
+        added to an vfs tqpt env-var stack. When lambdavar is then called on for that
+        env-var entry, Staqtapp builds a custom slots performance py module to run the
+        lambda parameterless. Converts all parameters of the lambda to slots attrs. It
+        allows lambda functions to be stored & ran this way from any sqtpp vfs files.
+        Once the lambda is ran once, then is direct ran without any conversion or edit
+        of the module. This not only allows memory reduction cuts for lambda functions
+        use but a one module container for any lambda function usage. The lambdalist
+        function allows specific param return reads of stored lambdas in a tqpt stack,
+        to be used in pairing with the awesome lambdavar function call.
+        
+     ____________________________________________________________________________________
+    """,
+    keywords = ['vfs env-var', 'stalkvar', 'lockvar', 'revar', 'addbranch_stx'],
+    url = 'https://github.com/lastforkbender/staqtapp',
+    zip_safe = False,
+    platforms = ['Linux', 'MacOS-X', 'Unix'],
+    license = 'Apache Software License',
+    classifiers = [
+        'Development Status :: PUBLIC DOMAIN SQTPP VER1.2 - VFS Variable Types Research/Stable or Hybrid-Q Tree Variable Types Research/Semi-Stable',
+        'Intended Audience :: Developers or Security Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Hybrid VFS Embedded Env-Var Software Development :: Libraries :: Python Modules',
+    ],
+)
